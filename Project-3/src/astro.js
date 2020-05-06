e4eec16816d94f55b86cc418dc6c321b5eb82d5e
Diana Diaz <dxd9318@g.rit.edu>
2020-05-06 02:51:14 -0400

    Finished!

--- Project-3/src/astro.js
+++ Project-3/src/astro.js
@@ -1,13 +1,54 @@
-const astroAccessToken = "85bf857892c9494d9ae67595e89c0b75";
+import { geojson } from "./map.js";
 
-function getAstroData(coord = [0,0]) {
-    // REMEMBER TO SWITCH LNG AND LAT WHEN INPUTING!!!
+const astroAccessToken = "85bf857892c9494d9ae67595e89c0b75";
+let locationRef = null;
+
+function composeAPICall(coord = [0, 0], arrayPos) {
+    locationRef = geojson.features[arrayPos].properties.description;
 
     let url = "https://api.ipgeolocation.io/astronomy?apiKey=";
     url += astroAccessToken;
-    url += "&lat=" + coord[1] + "&long=" + coord[0];
+    url += "&lat=" + coord[1] + "&long=" + coord[0];    // have to switch lat and long
 
-    console.log(url);
+    //console.log(url);
+    document.querySelector("#astralResults p").innerHTML = "Fetching sun and moon position data relative to " + locationRef + "...";
+
+    getData(url);
 }
 
-export {getAstroData};
+function getData(url) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = dataLoaded;
+    xhr.onerror = dataError;
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function dataLoaded(e) {
+    let xhr = e.target;
+
+    let obj = JSON.parse(xhr.responseText);
+    if (!obj || obj.length == 0) {
+        document.querySelector("#astralResults p").innerHTML = "Sorry, no results found for " + locationRef;
+        return;
+    }
+
+    let bigString = `Success! Relative to ${locationRef} on ${obj.date}:
+        <br>The Sun:<br>
+        The sun rises at ${obj.sunrise} and sets at ${obj.sunset}.<br> 
+        Solar noon is at ${obj.solar_noon}, providing ${obj.day_length} hours of sunlight.<br>
+        The sun is ${obj.sun_distance} km away from this point on Earth.<br><br>
+
+        The Moon:<br>
+        The moon rises at ${obj.moonrise} and sets at ${obj.moonset}.<br>
+        The moon is ${obj.moon_distance} km away from this point on Earth.<br>`;
+
+    document.querySelector("#astralResults p").innerHTML = bigString;
+}
+
+function dataError(e) {
+    console.log("An error occured");
+}
+
+export { composeAPICall };
--- Project-3/src/main.js
+++ Project-3/src/main.js
@@ -11,59 +11,69 @@ function init() {
 
 function setupUI() {
     let coords = [-21.4453125, 38.272688535980976];
+    let arrayPos = null;
     
     SF.onclick = () => {
         coords = map.geojson.features[0].geometry.coordinates;
+        arrayPos = 0;
 
         map.setZoomLevel(12);
         map.flyTo(coords);
-        astro.getAstroData(coords);
+        astro.composeAPICall(coords, arrayPos);
     }
     NY.onclick = () => {
         coords = map.geojson.features[1].geometry.coordinates;
+        arrayPos = 1;
 
         map.setZoomLevel(12);
         map.flyTo(coords);
-        astro.getAstroData(coords);
+        astro.composeAPICall(coords, arrayPos);
     }
     RJ.onclick = () => {
         coords = map.geojson.features[2].geometry.coordinates;
+        arrayPos = 2;
 
         map.setZoomLevel(12);
         map.flyTo(coords);
-        astro.getAstroData(coords);
+        astro.composeAPICall(coords, arrayPos);
     }
     LN.onclick = () => {
         coords = map.geojson.features[3].geometry.coordinates;
+        arrayPos = 3;
 
         map.setZoomLevel(12);
         map.flyTo(coords);
-        astro.getAstroData(coords);
+        astro.composeAPICall(coords, arrayPos);
     }
     AN.onclick = () => {
         coords = map.geojson.features[4].geometry.coordinates;
+        arrayPos = 4;
 
         map.setZoomLevel(12);
         map.flyTo(coords);
-        astro.getAstroData(coords);
+        astro.composeAPICall(coords, arrayPos);
     }
     HK.onclick = () => {
         coords = map.geojson.features[5].geometry.coordinates;
+        arrayPos = 5;
 
         map.setZoomLevel(12);
         map.flyTo(coords);
-        astro.getAstroData(coords);
+        astro.composeAPICall(coords, arrayPos);
     }
     SD.onclick = () => {
         coords = map.geojson.features[6].geometry.coordinates;
+        arrayPos = 6;
 
         map.setZoomLevel(12);
         map.flyTo(coords);
-        astro.getAstroData(coords);
+        astro.composeAPICall(coords, arrayPos);
     }
 
     resetMap.onclick = () => {
         coords = [-21.4453125, 38.272688535980976];
+        arrayPos = null;
+        
         map.setZoomLevel(1);
         map.flyTo(coords);
     }
